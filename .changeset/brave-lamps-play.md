973c8c0415bfa40365a2e363cc60e21503904a8b
Marc MacLeod <847542+marbemac@users.noreply.github.com>
2024-02-02 10:36:53 -0600

    fix(solid): run stream in request event async ctx
    Signed-off-by: Marc MacLeod <847542+marbemac@users.noreply.github.com>

---
+++ .changeset/brave-lamps-play.md
@@ -0,0 +1,5 @@
+---
+'@ssrx/solid': patch
+---
+
+Wrap the stream in the solid request event async ctx so that cache function from solid-router can be used in v0.11+.
--- packages/solid/src/server/stream.ts
+++ packages/solid/src/server/stream.ts
@@ -1,6 +1,8 @@
 import type { RenderToStreamFn } from '@ssrx/renderer/server';
 import { injectIntoStream } from '@ssrx/streaming';
 import { renderToStream as renderToSolidStream } from 'solid-js/web';
+/* @ts-expect-error no types */
+import { provideRequestEvent } from 'solid-js/web/storage';
 
 export const renderToStream: RenderToStreamFn<{
   nonce?: string;
@@ -8,12 +10,14 @@ export const renderToStream: RenderToStreamFn<{
   onCompleteShell?: (info: { write: (v: string) => void }) => void;
   onCompleteAll?: (info: { write: (v: string) => void }) => void;
 }> = async ({ req, app, injectToStream, opts }) => {
-  const stream = renderToSolidStream(() => app(), opts);
-  const { readable, writable } = new TransformStream();
-  stream.pipeTo(writable);
+  return provideRequestEvent(req, () => {
+    const stream = renderToSolidStream(() => app(), opts);
+    const { readable, writable } = new TransformStream();
+    stream.pipeTo(writable);
 
-  return {
-    stream: injectToStream ? injectIntoStream(req, readable, injectToStream) : readable,
-    statusCode: () => 200,
-  };
+    return {
+      stream: injectToStream ? injectIntoStream(req, readable, injectToStream) : readable,
+      statusCode: () => 200,
+    };
+  });
 };
