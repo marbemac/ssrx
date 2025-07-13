ea5ef30109fd3132633bd9627aac15e03daf481c
Marc MacLeod <marbemac+gh@gmail.com>
2025-07-13 17:44:24 -0500

    fix: fall back to vite’s default target in non ssr
    in vite < 7, it’s modules anyways. in vite > 7, modules is no longer an option, instead it’s “baseline-widely-available”
    
    Signed-off-by: Marc MacLeod <marbemac+gh@gmail.com>

--- packages/vite/src/config.ts
+++ packages/vite/src/config.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import type { MinimalPluginContextWithoutEnvironment, SSROptions } from 'vite';
-import { normalizePath } from 'vite';
+import { normalizePath, version } from 'vite';
 
 import type { ServerRuntime } from './types';
 
@@ -131,6 +131,10 @@ export class Config {
 
   get basePath() {
     return this.#basePath;
+  }
+
+  get viteMajorVersion() {
+    return Number(version.split('.')[0]);
   }
 
   /**
